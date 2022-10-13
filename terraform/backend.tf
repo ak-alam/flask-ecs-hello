9f7e0d26ea78d41d18f287258da15b50bab19bc4
ak-alam <alam.akbar1989@gmail.com>
2022-10-13 18:26:16 +0500

    terraform module pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 # terraform {
 #   backend "s3" {
-bucket         = "ak-terraform-backend"
-key            = "terraform.tfstate"
-region         = "us-west-2"
+bucket = "ak-terraform-backend"
+key = "terraform.tfstate"
+region = "us-west-2"
 dynamodb_table = "ak-terraform-backend-table"
 #   }
 # }
