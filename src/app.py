901868c17fd8aadeb3878744b730a9ac95413fe7
ak-alam <alam.akbar1989@gmail.com>
2022-10-15 02:20:01 +0500

    flask pipeline test

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def welcome():
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "I'm Github Action."
+    return "I'm Github Action.."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
