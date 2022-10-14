659e1b768e0e07feb6b7b5aa19adced8173941a9
ak-alam <alam.akbar1989@gmail.com>
2022-10-14 23:24:40 +0500

    terraform workflow testing

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,12 @@
-terraform {
-backend "s3" {
-bucket = "ak-terraform-backend"
+# terraform {
+# backend "s3" {
+# bucket = "ak-terraform-backend"
+# key = "terraform.tfstate"
+# region = "us-west-2"
+# dynamodb_table = "ak-terraform-backend-table"
+#   }
+# }
+bucket = "hello-flask-github-action"
 key = "terraform.tfstate"
 region = "us-west-2"
-dynamodb_table = "ak-terraform-backend-table"
-  }
-}
-
-
+dynamodb_table = "terraform.tfstate"
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,69 +9,69 @@ module "vpc" {
   prefix = "akbar"
 }
 
-module "lb-Sg" {
-  source = "./modules/sg"
-  vpc_id = module.vpc.vpcId
-  ingress_traffic = var.lb_IngressTraffic
-  protocol = "tcp"
-  prefix = "lb"
-}
+# module "lb-Sg" {
+#   source = "./modules/sg"
+#   vpc_id = module.vpc.vpcId
+#   ingress_traffic = var.lb_IngressTraffic
+#   protocol = "tcp"
+#   prefix = "lb"
+# }
 
-module "ecs_sg"{
-  source = "./modules/sg"
-  vpc_id = module.vpc.vpcId
-  ingress_traffic = var.ecs_IngressTraffic
-  protocol = "tcp"
-  prefix = "ecs"
-}
+# module "ecs_sg"{
+#   source = "./modules/sg"
+#   vpc_id = module.vpc.vpcId
+#   ingress_traffic = var.ecs_IngressTraffic
+#   protocol = "tcp"
+#   prefix = "ecs"
+# }
 
-module "applicationLB" {
-  source = "./modules/lb"
-  internal = false 
-  type = "application"
-  security_groups = ["${module.lb-Sg.security_group}"]
-  subne_Ids = module.vpc.public_subnet
-  vpc_Id = module.vpc.vpcId
-  deregistration_delay = var.deregistration_delay
-  tg_vars = local.alb.1
-  prefix = "alb"
+# module "applicationLB" {
+#   source = "./modules/lb"
+#   internal = false 
+#   type = "application"
+#   security_groups = ["${module.lb-Sg.security_group}"]
+#   subne_Ids = module.vpc.public_subnet
+#   vpc_Id = module.vpc.vpcId
+#   deregistration_delay = var.deregistration_delay
+#   tg_vars = local.alb.1
+#   prefix = "alb"
 
-}
+# }
 
-module "ecr" {
-  source = "./modules/ecr"
-  ecr_name = var.ecr_name
-}
-module "ecs_task_execution_role" {
-  source = "./modules/roles"
-  task_role_name = var.task_role_name
-}
+# module "ecr" {
+#   source = "./modules/ecr"
+#   ecr_name = var.ecr_name
+# }
+# module "ecs_task_execution_role" {
+#   source = "./modules/roles"
+#   task_role_name = var.task_role_name
+# }
 
-module "ecs" {
-  source = "./modules/ecs"
-  cluster_name = var.cluster_name
+# module "ecs" {
+#   source = "./modules/ecs"
+#   cluster_name = var.cluster_name
   
-  image_url = module.ecr.ecr_uri
-  task_execution_role = module.ecs_task_execution_role.ecs_tasks_role
-  # fargate_cpu = var.fargate_cpu
-  # fargate_memory = var.fargate_memory
-  # container_name = var.container_name
-  # task_definition_name = var.task_definition_name
-  # container_port = var.container_port
-  # host_port = var.host_port
-  container_definitions = {
-  fargate_cpu = var.container_definitions["fargate_cpu"]
-  fargate_memory = var.container_definitions["fargate_memory"]
-  container_name = var.container_definitions["container_name"]
-  task_definition_name = var.container_definitions["task_definition_name"]
-  container_port = var.container_definitions["container_port"]
-  host_port = var.container_definitions["host_port"]
-  }
-  service_vars = {
-    service_name = var.service_vars["service_name"]
-    desired_count = var.service_vars["desired_count"]
-  }
-  ecs_sg = module.ecs_sg.security_group
-  target_group = module.applicationLB.target_arns
-  subnets_id = module.vpc.public_subnet
-}
+#   image_url = module.ecr.ecr_uri
+#   task_execution_role = module.ecs_task_execution_role.ecs_tasks_role
+#   # fargate_cpu = var.fargate_cpu
+#   # fargate_memory = var.fargate_memory
+#   # container_name = var.container_name
+#   # task_definition_name = var.task_definition_name
+#   # container_port = var.container_port
+#   # host_port = var.host_port
+#   container_definitions = {
+#   fargate_cpu = var.container_definitions["fargate_cpu"]
+#   fargate_memory = var.container_definitions["fargate_memory"]
+#   container_name = var.container_definitions["container_name"]
+#   task_definition_name = var.container_definitions["task_definition_name"]
+#   container_port = var.container_definitions["container_port"]
+#   host_port = var.container_definitions["host_port"]
+#   }
+#   service_vars = {
+#     service_name = var.service_vars["service_name"]
+#     desired_count = var.service_vars["desired_count"]
+#   }
+#   ecs_sg = module.ecs_sg.security_group
+#   target_group = module.applicationLB.target_arns
+#   subnets_id = module.vpc.public_subnet
+# }
